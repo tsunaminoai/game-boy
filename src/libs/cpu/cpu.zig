2e046c7831e94ea8c1df703e9ba02c275f6f1e27
Tsunami Noai <spam@falseblue.com>
2023-08-11 14:06:56 -0400

    Refactor tests out

--- src/libs/cpu/cpu.zig
+++ src/libs/cpu/cpu.zig
@@ -1,21 +1,8 @@
 const std = @import("std");
+const Register = @import("types.zig").Register;
+const Flag = @import("types.zig").Flag;
 
 const MemorySize = 80000;
-
-const Register = enum(u8) {
-    // zig fmt: off
-    AF, BC, DE, HL,         //16bit "combined" registers
-    A, F, B, C, D, E, H, L, //"8bit" registers
-    SP                  //16bit special registers
-    // zig fmt: on
-};
-
-const Flag = enum(u4) {
-    Zero,
-    Subtraction,
-    HalfCarry,
-    Carry,
-};
 
 pub const CPU = struct {
     memory: [MemorySize]u8 = [_]u8{0} ** MemorySize,
@@ -89,38 +76,16 @@ pub const CPU = struct {
             Register.SP => {},
         }
     }
-    test "Test a register can be written to" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.AF, @as(u16, 0x0A0B));
-        try std.testing.expect(cpu.registers[@intFromEnum(Register.AF)] == 0x0A0B);
-    }
-    test "Test that writing to a sub-register writes to the parent and vice versa" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.AF, @as(u16, 0x0A0B));
-        try std.testing.expect(cpu.registers[@intFromEnum(Register.A)] == 0x0A);
-        try std.testing.expect(cpu.registers[@intFromEnum(Register.F)] == 0x0B);
 
-        cpu.RegisterWrite(Register.C, @as(u16, 0x0B));
-        cpu.RegisterWrite(Register.B, @as(u16, 0x0A));
-        try std.testing.expect(cpu.registers[@intFromEnum(Register.BC)] == 0x0A0B);
-    }
-
-    fn FlagSet(self: *Self, flag: Flag) void {
+    pub fn FlagSet(self: *Self, flag: Flag) void {
         self.flags[@intFromEnum(flag)] = true;
     }
 
-    fn FlagUnSet(self: *Self, flag: Flag) void {
+    pub fn FlagUnSet(self: *Self, flag: Flag) void {
         self.flags[@intFromEnum(flag)] = false;
     }
     pub fn FlagRead(self: *Self, flag: Flag) bool {
         return self.flags[@intFromEnum(flag)];
-    }
-    test "Test that flags can be set and unset" {
-        var cpu = CPU{};
-        cpu.FlagSet(Flag.Zero);
-        try std.testing.expect(cpu.FlagRead(Flag.Zero) == true);
-        cpu.FlagUnSet(Flag.Zero);
-        try std.testing.expect(cpu.FlagRead(Flag.Zero) == false);
     }
 
     fn LoadRegister(self: *Self, register: Register) void {
@@ -150,7 +115,7 @@ pub const CPU = struct {
         self.memory[address] = @as(u8, @truncate(self.RegisterRead(sourceRegister)));
     }
 
-    fn Tick(self: *Self) void {
+    pub fn Tick(self: *Self) void {
         self.dump("Start");
         const opcode = self.memory[self.programCounter];
         self.programCounter += 1;
@@ -263,99 +228,11 @@ pub const CPU = struct {
 
         self.dump("End");
     }
-    fn RegisterIncrement(self: *Self, register: Register) void {
+    pub fn RegisterIncrement(self: *Self, register: Register) void {
         self.registers[@intFromEnum(register)] +%= 0x1;
     }
-    fn RegisterDecrement(self: *Self, register: Register) void {
+    pub fn RegisterDecrement(self: *Self, register: Register) void {
         self.registers[@intFromEnum(register)] -%= 0x1;
-    }
-
-    test "Test ticking increments PC" {
-        var cpu = CPU{};
-        const pc = cpu.programCounter;
-        cpu.Tick();
-        try std.testing.expect(cpu.programCounter == pc + 1);
-    }
-    test "Test LD n,nn" {
-        var cpu = CPU{};
-        cpu.memory[0] = 0x06; // LDB,d8
-        cpu.memory[1] = 0xFE;
-        cpu.Tick();
-        try std.testing.expect(cpu.RegisterRead(Register.B) == 0xFE);
-    }
-    test "Test LD r1,r2" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.A, 0x0);
-        cpu.RegisterWrite(Register.H, 0xBC);
-        cpu.memory[0] = 0x7C; //LDA,L
-        cpu.Tick();
-        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBC);
-    }
-    test "Test LD r1,r2 with address register" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.A, 0x0);
-        cpu.RegisterWrite(Register.HL, 0xFFE);
-        cpu.memory[0xFFE] = 0xBE;
-
-        cpu.memory[0] = 0x7E; //LD A,(HL)
-        cpu.Tick();
-        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBE);
-    }
-    test "Test LD A,n with address register" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.A, 0x0);
-        cpu.RegisterWrite(Register.BC, 0xFF7);
-        cpu.memory[0xFF7] = 0xBE;
-
-        cpu.memory[0] = 0x0A; //LD A,(HL)
-        cpu.Tick();
-        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBE);
-    }
-    test "Test LD A,(nn)" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.A, 0x0);
-        cpu.memory[0xFF7] = 0xBE;
-
-        cpu.memory[0] = 0xFA; //LD A,(nn)
-        cpu.memory[1] = 0xF7;
-        cpu.memory[2] = 0x0F;
-        cpu.Tick();
-        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBE);
-    }
-    test "Test LD (nn),A" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.A, 0xBE);
-        cpu.memory[0xFF7] = 0x0;
-
-        cpu.memory[0] = 0xEA; //LD (nn),A
-        cpu.memory[1] = 0xF7;
-        cpu.memory[2] = 0x0F;
-        cpu.Tick();
-        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBE);
-    }
-    test "Test LDD A,(HL)" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.A, 0x0);
-        cpu.memory[0xFF7] = 0xBE;
-        cpu.RegisterWrite(Register.HL, 0x0FF7);
-
-        cpu.memory[0] = 0x3A;
-
-        cpu.Tick();
-        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBE);
-        try std.testing.expect(cpu.RegisterRead(Register.HL) == 0x0FF6);
-    }
-    test "Test LDD (HL),A" {
-        var cpu = CPU{};
-        cpu.RegisterWrite(Register.A, 0x00BE);
-        cpu.memory[0x0040] = 0x0;
-        cpu.RegisterWrite(Register.HL, 0x0040);
-
-        cpu.memory[0] = 0x32;
-
-        cpu.Tick();
-        try std.testing.expect(cpu.memory[0x0040] == 0x00BE);
-        try std.testing.expect(cpu.RegisterRead(Register.HL) == 0x003F);
     }
 
     pub fn dump(self: *Self, msg: []const u8) void {
@@ -382,3 +259,7 @@ pub const CPU = struct {
         std.debug.print("\n====  {s}  ====\n", .{msg});
     }
 };
+
+test {
+    _ = @import("tests.zig");
+}
---
+++ src/libs/cpu/types.zig
@@ -0,0 +1,14 @@
+pub const Register = enum(u8) {
+    // zig fmt: off
+    AF, BC, DE, HL,         //16bit "combined" registers
+    A, F, B, C, D, E, H, L, //"8bit" registers
+    SP                  //16bit special registers
+    // zig fmt: on
+};
+
+pub const Flag = enum(u4) {
+    Zero,
+    Subtraction,
+    HalfCarry,
+    Carry,
+};
