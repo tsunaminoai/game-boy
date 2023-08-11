d714a9b7d78d9c5239356d51f5857c8088c685a5
Tsunami Noai <spam@falseblue.com>
2023-08-11 08:47:49 -0400

    Working registers!

--- src/libs/cpu/cpu.zig
+++ src/libs/cpu/cpu.zig
@@ -37,58 +37,61 @@ const CPU = struct {
         return self.registers[@intFromEnum(register)];
     }
 
-    pub fn RegisterWrite(self: *Self, register: Register, value: anytype) void {
-        std.debug.print("RegisterWrite({},{}: {X})\n", .{ register, @TypeOf(value), value });
+    fn internalRegisterWrite(self: *Self, register: Register, value: u16) void {
         self.registers[@intFromEnum(register)] = value;
+    }
 
+    pub fn RegisterWrite(self: *Self, register: Register, value: u16) void {
+        // std.debug.print("RegisterWrite({},{}: {X})\n", .{ register, @TypeOf(value), value });
+        self.internalRegisterWrite(register, value);
         switch (register) {
             Register.AF => {
-                self.RegisterWrite(Register.A, value >> 8);
-                self.RegisterWrite(Register.F, value & 0x0F);
+                self.internalRegisterWrite(Register.A, value >> 8);
+                self.internalRegisterWrite(Register.F, value & 0x0F);
             },
             Register.BC => {
-                self.RegisterWrite(Register.B, value >> 8);
-                self.RegisterWrite(Register.C, value & 0x0F);
+                self.internalRegisterWrite(Register.B, value >> 8);
+                self.internalRegisterWrite(Register.C, value & 0x0F);
             },
             Register.DE => {
-                self.RegisterWrite(Register.D, value >> 8);
-                self.RegisterWrite(Register.E, value & 0x0F);
+                self.internalRegisterWrite(Register.D, value >> 8);
+                self.internalRegisterWrite(Register.E, value & 0x0F);
             },
             Register.HL => {
-                self.RegisterWrite(Register.H, value >> 8);
-                self.RegisterWrite(Register.L, value & 0x0F);
+                self.internalRegisterWrite(Register.H, value >> 8);
+                self.internalRegisterWrite(Register.L, value & 0x0F);
             },
             Register.A => {
                 const currentValue = self.RegisterRead(Register.AF);
-                self.RegisterWrite(Register.AF, (value << 8) & currentValue);
+                self.internalRegisterWrite(Register.AF, (value << 8) | currentValue);
             },
             Register.F => {
                 const currentValue = self.RegisterRead(Register.AF);
-                self.RegisterWrite(Register.AF, (value & 0x0F) & currentValue);
+                self.internalRegisterWrite(Register.AF, (value & 0x0F) | currentValue);
             },
             Register.B => {
                 const currentValue = self.RegisterRead(Register.BC);
-                self.RegisterWrite(Register.BC, (value << 8) & currentValue);
+                self.internalRegisterWrite(Register.BC, (value << 8) | currentValue);
             },
             Register.C => {
                 const currentValue = self.RegisterRead(Register.BC);
-                self.RegisterWrite(Register.BC, (value & 0x0F) & currentValue);
+                self.internalRegisterWrite(Register.BC, (value & 0x0F) | currentValue);
             },
             Register.D => {
                 const currentValue = self.RegisterRead(Register.DE);
-                self.RegisterWrite(Register.DE, (value << 8) & currentValue);
+                self.internalRegisterWrite(Register.DE, (value << 8) | currentValue);
             },
             Register.E => {
                 const currentValue = self.RegisterRead(Register.DE);
-                self.RegisterWrite(Register.DE, (value & 0x0F) & currentValue);
+                self.internalRegisterWrite(Register.DE, (value & 0x0F) | currentValue);
             },
             Register.H => {
                 const currentValue = self.RegisterRead(Register.HL);
-                self.RegisterWrite(Register.HL, (value << 8) & currentValue);
+                self.internalRegisterWrite(Register.HL, (value << 8) | currentValue);
             },
             Register.L => {
                 const currentValue = self.RegisterRead(Register.HL);
-                self.RegisterWrite(Register.HL, (value & 0x0F) & currentValue);
+                self.internalRegisterWrite(Register.HL, (value & 0x0F) | currentValue);
             },
             else => unreachable,
         }
@@ -104,8 +107,9 @@ test "Test that writing to a sub-register writes to the parent and vice versa" {
     var cpu = CPU{};
     cpu.RegisterWrite(Register.AF, @as(u16, 0x0A0B));
     try std.testing.expect(cpu.registers[@intFromEnum(Register.A)] == 0x0A);
+    try std.testing.expect(cpu.registers[@intFromEnum(Register.F)] == 0x0B);
 
-    cpu.RegisterWrite(Register.C, @as(u8, 0x0B));
-    cpu.RegisterWrite(Register.B, @as(u8, 0x0A));
+    cpu.RegisterWrite(Register.C, @as(u16, 0x0B));
+    cpu.RegisterWrite(Register.B, @as(u16, 0x0A));
     try std.testing.expect(cpu.registers[@intFromEnum(Register.BC)] == 0x0A0B);
 }
