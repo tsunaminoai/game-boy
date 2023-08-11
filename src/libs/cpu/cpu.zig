6bd3b91b7549c1f38bd7719640c46f0b473898ff
Tsunami Noai <spam@falseblue.com>
2023-08-11 11:06:27 -0400

    Add LD r1,r2

--- src/libs/cpu/cpu.zig
+++ src/libs/cpu/cpu.zig
@@ -126,6 +126,9 @@ pub const CPU = struct {
     fn LoadRegister(self: *Self, register: Register) void {
         self.RegisterWrite(register, self.memory[self.programCounter]);
     }
+    fn LoadRegisterFromRegister(self: *Self, source: Register, destination: Register) void {
+        self.RegisterWrite(destination, self.RegisterRead(source));
+    }
     fn LoadRegisterImmediate(self: *Self, register: Register, value: u16) void {
         self.RegisterWrite(register, value);
     }
@@ -143,6 +146,72 @@ pub const CPU = struct {
             0x26 => { self.LoadRegister(Register.H); },
             0x2E => { self.LoadRegister(Register.L); },
 
+            //LD r1,r2
+            0x7F => { self.LoadRegisterFromRegister(Register.A,Register.A); },
+            0x78 => { self.LoadRegisterFromRegister(Register.B,Register.A); },
+            0x79 => { self.LoadRegisterFromRegister(Register.C,Register.A); },
+            0x7A => { self.LoadRegisterFromRegister(Register.D,Register.A); },
+            0x7B => { self.LoadRegisterFromRegister(Register.E,Register.A); },
+            0x7C => { self.LoadRegisterFromRegister(Register.H,Register.A); },
+            0x7D => { self.LoadRegisterFromRegister(Register.L,Register.A); },
+            0x7E => { self.LoadRegisterFromRegister(Register.HL, Register.A); },
+
+            0x40 => { self.LoadRegisterFromRegister(Register.B, Register.B); },
+            0x41 => { self.LoadRegisterFromRegister(Register.C, Register.B); },
+            0x42 => { self.LoadRegisterFromRegister(Register.D, Register.B); },
+            0x43 => { self.LoadRegisterFromRegister(Register.E, Register.B); },
+            0x44 => { self.LoadRegisterFromRegister(Register.H, Register.B); },
+            0x45 => { self.LoadRegisterFromRegister(Register.L, Register.B); },
+            0x46 => { self.LoadRegisterFromRegister(Register.HL, Register.B); },
+
+            0x48 => { self.LoadRegisterFromRegister(Register.B, Register.C); },
+            0x49 => { self.LoadRegisterFromRegister(Register.C, Register.C); },
+            0x4A => { self.LoadRegisterFromRegister(Register.D, Register.C); },
+            0x4B => { self.LoadRegisterFromRegister(Register.E, Register.C); },
+            0x4C => { self.LoadRegisterFromRegister(Register.H, Register.C); },
+            0x4D => { self.LoadRegisterFromRegister(Register.L, Register.C); },
+            0x4E => { self.LoadRegisterFromRegister(Register.HL, Register.C); },
+
+            0x50 => { self.LoadRegisterFromRegister(Register.B, Register.D); },
+            0x51 => { self.LoadRegisterFromRegister(Register.C, Register.D); },
+            0x52 => { self.LoadRegisterFromRegister(Register.D, Register.D); },
+            0x53 => { self.LoadRegisterFromRegister(Register.E, Register.D); },
+            0x54 => { self.LoadRegisterFromRegister(Register.H, Register.D); },
+            0x55 => { self.LoadRegisterFromRegister(Register.L, Register.D); },
+            0x56 => { self.LoadRegisterFromRegister(Register.HL, Register.D); },
+
+            0x58 => { self.LoadRegisterFromRegister(Register.B, Register.E); },
+            0x59 => { self.LoadRegisterFromRegister(Register.C, Register.E); },
+            0x5A => { self.LoadRegisterFromRegister(Register.D, Register.E); },
+            0x5B => { self.LoadRegisterFromRegister(Register.E, Register.E); },
+            0x5C => { self.LoadRegisterFromRegister(Register.H, Register.E); },
+            0x5D => { self.LoadRegisterFromRegister(Register.L, Register.E); },
+            0x5E => { self.LoadRegisterFromRegister(Register.HL, Register.E); },
+
+            0x60 => { self.LoadRegisterFromRegister(Register.B, Register.H); },
+            0x61 => { self.LoadRegisterFromRegister(Register.C, Register.H); },
+            0x62 => { self.LoadRegisterFromRegister(Register.D, Register.H); },
+            0x63 => { self.LoadRegisterFromRegister(Register.E, Register.H); },
+            0x64 => { self.LoadRegisterFromRegister(Register.H, Register.H); },
+            0x65 => { self.LoadRegisterFromRegister(Register.L, Register.H); },
+            0x66 => { self.LoadRegisterFromRegister(Register.HL, Register.H); },
+
+            0x68 => { self.LoadRegisterFromRegister(Register.B, Register.L); },
+            0x69 => { self.LoadRegisterFromRegister(Register.C, Register.L); },
+            0x6A => { self.LoadRegisterFromRegister(Register.D, Register.L); },
+            0x6B => { self.LoadRegisterFromRegister(Register.E, Register.L); },
+            0x6C => { self.LoadRegisterFromRegister(Register.H, Register.L); },
+            0x6D => { self.LoadRegisterFromRegister(Register.L, Register.L); },
+            0x6E => { self.LoadRegisterFromRegister(Register.HL, Register.L); },
+
+            0x70 => { self.LoadRegisterFromRegister(Register.B, Register.HL); },
+            0x71 => { self.LoadRegisterFromRegister(Register.C, Register.HL); },
+            0x72 => { self.LoadRegisterFromRegister(Register.D, Register.HL); },
+            0x73 => { self.LoadRegisterFromRegister(Register.E, Register.HL); },
+            0x74 => { self.LoadRegisterFromRegister(Register.H, Register.HL); },
+            0x75 => { self.LoadRegisterFromRegister(Register.L, Register.HL); },
+            0x36 => { self.LoadRegisterFromRegister(Register.L, Register.HL); },
+
             // zig fmt: on
             else => undefined,
         }
@@ -153,11 +222,19 @@ pub const CPU = struct {
         cpu.Tick();
         try std.testing.expect(cpu.programCounter == pc + 1);
     }
-    test "Test LD B,n" {
+    test "Test LD n,nn" {
         var cpu = CPU{};
-        cpu.memory[0] = 0x06;
+        cpu.memory[0] = 0x06; // LDB,d8
         cpu.memory[1] = 0xFE;
         cpu.Tick();
         try std.testing.expect(cpu.RegisterRead(Register.B) == 0xFE);
+    }
+    test "Test LD r1,r2" {
+        var cpu = CPU{};
+        cpu.RegisterWrite(Register.A, 0x0);
+        cpu.RegisterWrite(Register.H, 0xBC);
+        cpu.memory[0] = 0x7C; //LDA,L
+        cpu.Tick();
+        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBC);
     }
 };
