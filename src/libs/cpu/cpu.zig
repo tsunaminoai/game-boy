3ee6349f92c8fe448eea35b549c10364fbea87bb
Tsunami Noai <spam@falseblue.com>
2023-08-11 11:18:58 -0400

    Remember that (HL) means "the address at HL"

--- src/libs/cpu/cpu.zig
+++ src/libs/cpu/cpu.zig
@@ -129,6 +129,9 @@ pub const CPU = struct {
     fn LoadRegisterFromRegister(self: *Self, source: Register, destination: Register) void {
         self.RegisterWrite(destination, self.RegisterRead(source));
     }
+    fn LoadRegisterFromAddressRegister(self: *Self, source: Register, destination: Register) void {
+        self.RegisterWrite(destination, self.memory[self.RegisterRead(source)]);
+    }
     fn LoadRegisterImmediate(self: *Self, register: Register, value: u16) void {
         self.RegisterWrite(register, value);
     }
@@ -154,7 +157,7 @@ pub const CPU = struct {
             0x7B => { self.LoadRegisterFromRegister(Register.E,Register.A); },
             0x7C => { self.LoadRegisterFromRegister(Register.H,Register.A); },
             0x7D => { self.LoadRegisterFromRegister(Register.L,Register.A); },
-            0x7E => { self.LoadRegisterFromRegister(Register.HL, Register.A); },
+            0x7E => { self.LoadRegisterFromAddressRegister(Register.HL, Register.A); },
 
             0x40 => { self.LoadRegisterFromRegister(Register.B, Register.B); },
             0x41 => { self.LoadRegisterFromRegister(Register.C, Register.B); },
@@ -162,7 +165,7 @@ pub const CPU = struct {
             0x43 => { self.LoadRegisterFromRegister(Register.E, Register.B); },
             0x44 => { self.LoadRegisterFromRegister(Register.H, Register.B); },
             0x45 => { self.LoadRegisterFromRegister(Register.L, Register.B); },
-            0x46 => { self.LoadRegisterFromRegister(Register.HL, Register.B); },
+            0x46 => { self.LoadRegisterFromAddressRegister(Register.HL, Register.B); },
 
             0x48 => { self.LoadRegisterFromRegister(Register.B, Register.C); },
             0x49 => { self.LoadRegisterFromRegister(Register.C, Register.C); },
@@ -170,7 +173,7 @@ pub const CPU = struct {
             0x4B => { self.LoadRegisterFromRegister(Register.E, Register.C); },
             0x4C => { self.LoadRegisterFromRegister(Register.H, Register.C); },
             0x4D => { self.LoadRegisterFromRegister(Register.L, Register.C); },
-            0x4E => { self.LoadRegisterFromRegister(Register.HL, Register.C); },
+            0x4E => { self.LoadRegisterFromAddressRegister(Register.HL, Register.C); },
 
             0x50 => { self.LoadRegisterFromRegister(Register.B, Register.D); },
             0x51 => { self.LoadRegisterFromRegister(Register.C, Register.D); },
@@ -178,7 +181,7 @@ pub const CPU = struct {
             0x53 => { self.LoadRegisterFromRegister(Register.E, Register.D); },
             0x54 => { self.LoadRegisterFromRegister(Register.H, Register.D); },
             0x55 => { self.LoadRegisterFromRegister(Register.L, Register.D); },
-            0x56 => { self.LoadRegisterFromRegister(Register.HL, Register.D); },
+            0x56 => { self.LoadRegisterFromAddressRegister(Register.HL, Register.D); },
 
             0x58 => { self.LoadRegisterFromRegister(Register.B, Register.E); },
             0x59 => { self.LoadRegisterFromRegister(Register.C, Register.E); },
@@ -186,7 +189,7 @@ pub const CPU = struct {
             0x5B => { self.LoadRegisterFromRegister(Register.E, Register.E); },
             0x5C => { self.LoadRegisterFromRegister(Register.H, Register.E); },
             0x5D => { self.LoadRegisterFromRegister(Register.L, Register.E); },
-            0x5E => { self.LoadRegisterFromRegister(Register.HL, Register.E); },
+            0x5E => { self.LoadRegisterFromAddressRegister(Register.HL, Register.E); },
 
             0x60 => { self.LoadRegisterFromRegister(Register.B, Register.H); },
             0x61 => { self.LoadRegisterFromRegister(Register.C, Register.H); },
@@ -194,7 +197,7 @@ pub const CPU = struct {
             0x63 => { self.LoadRegisterFromRegister(Register.E, Register.H); },
             0x64 => { self.LoadRegisterFromRegister(Register.H, Register.H); },
             0x65 => { self.LoadRegisterFromRegister(Register.L, Register.H); },
-            0x66 => { self.LoadRegisterFromRegister(Register.HL, Register.H); },
+            0x66 => { self.LoadRegisterFromAddressRegister(Register.HL, Register.H); },
 
             0x68 => { self.LoadRegisterFromRegister(Register.B, Register.L); },
             0x69 => { self.LoadRegisterFromRegister(Register.C, Register.L); },
@@ -202,7 +205,7 @@ pub const CPU = struct {
             0x6B => { self.LoadRegisterFromRegister(Register.E, Register.L); },
             0x6C => { self.LoadRegisterFromRegister(Register.H, Register.L); },
             0x6D => { self.LoadRegisterFromRegister(Register.L, Register.L); },
-            0x6E => { self.LoadRegisterFromRegister(Register.HL, Register.L); },
+            0x6E => { self.LoadRegisterFromAddressRegister(Register.HL, Register.L); },
 
             0x70 => { self.LoadRegisterFromRegister(Register.B, Register.HL); },
             0x71 => { self.LoadRegisterFromRegister(Register.C, Register.HL); },
@@ -236,5 +239,15 @@ pub const CPU = struct {
         cpu.memory[0] = 0x7C; //LDA,L
         cpu.Tick();
         try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBC);
+    }
+    test "Test LD r1,r2 with address register" {
+        var cpu = CPU{};
+        cpu.RegisterWrite(Register.A, 0x0);
+        cpu.RegisterWrite(Register.HL, 0xFFE);
+        cpu.memory[0xFFE] = 0xBE;
+
+        cpu.memory[0] = 0x7E; //LD A,(HL)
+        cpu.Tick();
+        try std.testing.expect(cpu.RegisterRead(Register.A) == 0xBE);
     }
 };
